61e02aae594fcb328534b049e24b4a54691da2b7
Fakhri <64248021+FakhriMaulanaM@users.noreply.github.com>
2022-03-09 18:06:33 +0700

    Update and rename database snipeit.txt to database.sh

---
+++ database.sh
@@ -0,0 +1,1 @@
+docker run -d --name snipe-mysql -e MYSQL_ROOT_PASSWORD="jaya" -e MYSQL_DATABASE=snipe -e MYSQL_USER=snipe -e MYSQL_PASSWORD="dbjaya" -e TZ=Asia/Jakarta -p 127.0.0.1:3306:3306 -v /srv/config/snipeit/snipe-mysql:/var/lib/mysql mysql:5.6 --sql-mode=""
